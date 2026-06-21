c2c6c4d75f70801b94f543176befedeafc110c65
Francis Boisvert <333powametal@gmail.com>
2026-06-21 13:34:08 -0400

    [Bug] Correction du calcul de la moyenne de pages par livre

--- src/affichage/affichage_statistiques.rs
+++ src/affichage/affichage_statistiques.rs
@@ -1,6 +1,6 @@
 
 
-pub fn affichage_statistiques(total_livres: i32, total_pages: i32, moyenne_pages: i32, nombre_disponible: i32, nombre_emprunte: i32) {
+pub fn affichage_statistiques(total_livres: i32, total_pages: i32, moyenne_pages: f64, nombre_disponible: i32, nombre_emprunte: i32) {
 
 
     println!();
@@ -9,13 +9,13 @@ pub fn affichage_statistiques(total_livres: i32, total_pages: i32, moyenne_pages
     println!("{:^42}","+==========================================+");
     println!();
     print!("{:>31}", format!(" Nombre total de livres:"));
-    println!("{:>10.10}", total_livres);
+    println!("{:>10}", total_livres);
 
     print!("{:>31}", format!(" Nombre total de pages:" ));
-    println!("{:>10.10}", total_pages);
+    println!("{:>10}", total_pages);
 
     print!("{:>31}", format!(" Moyenne de pages par livre:" ));
-    println!("{:>10.10}", moyenne_pages);
+    println!("{:>10.2}", moyenne_pages);
 
 
     println!();
--- src/statistiques/stat_moyenne_pages.rs
+++ src/statistiques/stat_moyenne_pages.rs
@@ -1,12 +1,13 @@
 use crate::livres::bibliotheque::Bibliotheque;
 
 
-pub fn stat_moyenne_pages(bibliotheque: &Bibliotheque) -> i32{
-    let total_livres = bibliotheque.livres.len() as i32;
-    if total_livres == 0 {
-        return 0
+pub fn stat_moyenne_pages(bibliotheque: &Bibliotheque) -> f64{
+    let total_livres = bibliotheque.livres.len() as f64;
+    if total_livres == 0.0 {
+        return 0.0
     }
-    let total_pages: i32 = bibliotheque.livres.iter().map(|l| l.pages).sum();
+    let total_pages: f64  = bibliotheque.livres.iter().map(|l| l.pages).sum::<i32>() as f64;
+
     total_pages / total_livres
     
 }
